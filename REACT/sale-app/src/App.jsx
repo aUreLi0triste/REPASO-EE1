f99df35d88888192d03617305a33687f72cde756
aUreLi0_triste <aureliotriste088@gmail.com>
2025-10-30 22:05:37 -0500

    sale app parte 1

--- REACT/sale-app/src/App.jsx
+++ REACT/sale-app/src/App.jsx
@@ -1,14 +1,31 @@
 import './App.css'
-import { useState } from 'react' //importacion modular
+import { useState, useEffect, use} from 'react' //importacion modular
 import Gamecard from './components/Gamecard/Gamecard'
-import productos from './data/productosData'
+import productosOriginales from './data/productosData'
 
 function App() {
   const [busqueda, setBusqueda] = useState("") //devuelve un array con dos elementos: el estado actual y una funcion para actualizar ese estado
+  const [productos, setProductos] = useState(productosOriginales)
+
+  // useEffect(() => {
+  //   if(productos.length <= 0){
+  //     alert("No se encontraron productos")
+  //   }
+  // }, [productos])
+
+  useEffect(() => {
+    if(busqueda.length === 0){
+      setProductos(productosOriginales)
+    }
+    else if(busqueda.length > 3){
+      handleBuscar()
+      
+    }
+  }, [busqueda])
 
   const handleBuscar = () => {
-    productos.filter((item) => item.nombre.toLowerCase().includes(busqueda.toLowerCase()))
-
+    const filtrados = productosOriginales.filter((item) => item.nombre.toLowerCase().includes(busqueda.toLowerCase()))
+    setProductos(filtrados)
   }
 
   return (
@@ -17,20 +34,23 @@ function App() {
         <h1>Venta de garage</h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea soluta pariatur sequi fugit ipsam excepturi nostrum nam, labore, rem, sed unde hic. Nam, tempora officia. Placeat exercitationem nisi fugiat tempora.</p>
       </header>
+      <nav>
+        <a href="/acerca">Acerca de</a>
+      </nav>
+
+
       <main>
-        <section>
+        <section className='sectionSearch'>
           <input type="text" placeholder='Bucar por nombre...' value={busqueda} onChange={(event) => setBusqueda(event.target.value)} />
           <button onClick={() => handleBuscar()}>Buscar</button>
         </section>
-        <section>
+        <section className='sectionGames'>
           {
-            productos.map((item) => {
+            productos.length > 0 ? productos.map((item) => { //renderizado condicional
               return (
                 <Gamecard {...item} />
-
               )
-            }
-            )
+            }) : <p>No se encontraron productos</p> 
           }
 
         </section>
---
+++ REACT/sale-app/src/routes/AboutUs.jsx
@@ -0,0 +1,9 @@
+const AboutUs = () => { 
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>This is the About Us page of the Sale App.</p>
+        </div>
+    )
+}   
+export default AboutUs
